33a2f1e5b530f6e619ca3a0d37d154270761557f
vanrogh <k.elaman2002@gmail.com>
2023-11-29 22:07:38 +0600

    Code refactoring 2

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -23,18 +23,23 @@ export default class ItemList extends Component {
             })
     }
 
-    renderItems(arr) {
-        return arr.map(({id, name}) => {
+    renderItems = (arr) => {
+        return arr.map((item) => {
+            const {id} = item;
+
+            if(!this.props.renderItem) debugger;
+
+            const label = this.props.renderItem(item);
             return (
                 <li
                     className="list-group-item"
                     key={id}
                     onClick={ () => this.props.onItemSelected(id) }
                 >
-                    {name}
+                    {label}
                 </li>)
         });
-    }
+    };
 
 
     render() {
