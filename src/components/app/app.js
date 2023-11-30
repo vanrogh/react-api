4373ae98fcbea3affec73a469fb74b33dae43016
vanrogh <k.elaman2002@gmail.com>
2023-11-30 09:22:43 +0600

    I have done refactoring

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -75,17 +75,9 @@ export default class App extends Component{
                     <StarshipDetails itemId={5} />
                     <PlanetDetails itemId={9} />
 
-                    <PersonList>
-                        { ({name}) => <span>{name}</span> }
-                    </PersonList>
-
-                    <StarshipList>
-                        { ({name}) => <span>{name}</span> }
-                    </StarshipList>
-
-                    <PlanetList>
-                        { ({name}) => <span>{name}</span> }
-                    </PlanetList>
+                    <PersonList />
+                    <StarshipList />
+                    <PlanetList />
 
                     {/*<ItemList
                         getData={getAllPlanets}
